b11606b6c83925e9bf92ceccd3b3852dcb05e4af
tomo2k4 <mike19122004@gmail.com>
2024-11-08 13:59:17 +0700

    commit Trang Chu & icon

--- src/ui_library/frmTrangchu.java
+++ src/ui_library/frmTrangchu.java
@@ -32,11 +32,11 @@ public class frmTrangchu extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 1141, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGap(0, 825, Short.MAX_VALUE)
         );
 
         pack();
